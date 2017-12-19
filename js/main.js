cda1dad8a4d8255f35b3008a4bdbf9a252d0435f
Sahana <sahanakmr32@gmail.com>
2017-12-20 09:52:06 +1100

    js code optimised

--- js/main.js
+++ js/main.js
@@ -20,85 +20,94 @@ if(click % 2 === 0){
     displayO(e);
 
 }
-gameWonRows();
-gameWonCol();
-gameDiag();
+
+displayResult();
+
 click++
+
 });
 
 
-let gameWonRows = function(){
-let rowOne = $('.col1').text();
-let rowTwo = $('.col2').text();
-let rowThree = $('.col3').text();
 
-if(rowOne === "XXX"){
-  $('#X').show();
-}else if(rowOne === "OOO"){
-  $('#Y').show();
-}
+// let gameWonRows = function(){
+// let rowOne = $('.col1').text();
+// let rowTwo = $('.col2').text();
+// let rowThree = $('.col3').text();
+// let rowFour = $('.col4').text();
+// let rowFive = $('.col5').text();
+// let rowSix = $('.col6').text();
+// let rowSeven = $('.col7').text();
+// let rowEight = $('.col8').text();
+//
+// if(rowOne === "XXX"){
+//   $('#X').show();
+// }else if(rowOne === "OOO"){
+//   $('#Y').show();
+// }
+//
+// if(rowTwo === "XXX"){
+//   $('#X').show();
+// }else if(rowTwo === "OOO"){
+//   $('#Y').show();
+// }
+//
+// if(rowThree === "XXX"){
+//   $('#X').show();
+// }else if(rowThree === "OOO"){
+//   $('#Y').show();
+// }
+//
+// if(rowFour === "XXX"){
+//   $('#X').show();
+// }else if(rowFour === "OOO"){
+//   $('#Y').show();
+// }
+//
+// if(rowFive === "XXX"){
+//   $('#X').show();
+// }else if(rowFive === "OOO"){
+//   $('#Y').show();
+// }
+//
+// if(rowSix === "XXX"){
+//   $('#X').show();
+// }else if(rowSix === "OOO"){
+//   $('#Y').show();
+// }
+// if(rowSeven === "XXX"){
+//   $('#X').show();
+// }else if(rowSeven === "OOO"){
+//   $('#Y').show();
+// }
+//
+// if(rowEight === "XXX"){
+//   $('#X').show();
+// }else if(rowEight === "OOO"){
+//   $('#Y').show();
+// }
+// }
 
-if(rowTwo === "XXX"){
-  $('#X').show();
-}else if(rowTwo === "OOO"){
-  $('#Y').show();
-}
+ displayResult = function() {
 
-if(rowThree === "XXX"){
-  $('#X').show();
-}else if(rowThree === "OOO"){
-  $('#Y').show();
-}
-}
-
-let gameWonCol = function(){
-
-  let rowOne = $('.col4').text();
-  let rowTwo = $('.col5').text();
-  let rowThree = $('.col6').text();
-  if(rowOne === "XXX"){
-    $('#X').show();
-  }else if(rowOne === "OOO"){
-    $('#Y').show();
+   let gameRows = {
+    rowOne:$('.col1').text(),
+    rowTwo :$('.col2').text(),
+    rowThree:$('.col3').text(),
+    rowFour:$('.col4').text(),
+    rowFive:$('.col5').text(),
+    rowSix:$('.col6').text(),
+    rowSeven:$('.col7').text(),
+    rowEight:$('.col8').text()
   }
 
-  if(rowTwo === "XXX"){
-    $('#X').show();
-  }else if(rowTwo === "OOO"){
-    $('#Y').show();
+  for (let key in gameRows){
+    if(gameRows[key] === "XXX"){
+      $('#X').show();
+    }else if(gameRows[key] === "OOO"){
+      $('#Y').show();
+    }
   }
-
-  if(rowThree === "XXX"){
-    $('#X').show();
-  }else if(rowThree === "OOO"){
-    $('#Y').show();
-  }
-
-
-}
-
-let gameDiag = function(){
-  let rowOne = $('.col7').text();
-  let rowTwo = $('.col8').text();
-
-  if(rowOne === "XXX"){
-    $('#X').show();
-  }else if(rowOne === "OOO"){
-    $('#Y').show();
-  }
-
-  if(rowTwo === "XXX"){
-    $('#X').show();
-  }else if(rowTwo === "OOO"){
-    $('#Y').show();
-  }
-
-
-}
-
-
-
-
+ }
 
 
 
