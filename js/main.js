93de7159d279a20f422187f382e4b4e668fad71e
Sahana <sahanakmr32@gmail.com>
2017-12-20 10:41:37 +1100

    page reloads when the game is won or draw

--- js/main.js
+++ js/main.js
@@ -1,115 +1,54 @@
-$(document).ready(function(){
-let click = 1;
-
-
-
-  let displayX = function(e){
-    $( event.target ).html( "X");
+$(document).ready(function() {
+  let click = 1;
+  let displayX = function(e) {
+    $(event.target).html("X");
   }
 
-  let displayO = function(e){
-    $( event.target ).html( "O");
+  let displayO = function(e) {
+    $(event.target).html("O");
   }
 
-$( ".col" ).click(function( e ) {
-if(click % 2 === 0){
-
-    displayX(e);
-}else{
-
-    displayO(e);
-
-}
-
-displayResult();
-
-click++
-
-});
-
-
-
-// let gameWonRows = function(){
-// let rowOne = $('.col1').text();
-// let rowTwo = $('.col2').text();
-// let rowThree = $('.col3').text();
-// let rowFour = $('.col4').text();
-// let rowFive = $('.col5').text();
-// let rowSix = $('.col6').text();
-// let rowSeven = $('.col7').text();
-// let rowEight = $('.col8').text();
-//
-// if(rowOne === "XXX"){
-//   $('#X').show();
-// }else if(rowOne === "OOO"){
-//   $('#Y').show();
-// }
-//
-// if(rowTwo === "XXX"){
-//   $('#X').show();
-// }else if(rowTwo === "OOO"){
-//   $('#Y').show();
-// }
-//
-// if(rowThree === "XXX"){
-//   $('#X').show();
-// }else if(rowThree === "OOO"){
-//   $('#Y').show();
-// }
-//
-// if(rowFour === "XXX"){
-//   $('#X').show();
-// }else if(rowFour === "OOO"){
-//   $('#Y').show();
-// }
-//
-// if(rowFive === "XXX"){
-//   $('#X').show();
-// }else if(rowFive === "OOO"){
-//   $('#Y').show();
-// }
-//
-// if(rowSix === "XXX"){
-//   $('#X').show();
-// }else if(rowSix === "OOO"){
-//   $('#Y').show();
-// }
-// if(rowSeven === "XXX"){
-//   $('#X').show();
-// }else if(rowSeven === "OOO"){
-//   $('#Y').show();
-// }
-//
-// if(rowEight === "XXX"){
-//   $('#X').show();
-// }else if(rowEight === "OOO"){
-//   $('#Y').show();
-// }
-// }
-
- displayResult = function() {
-
-   let gameRows = {
-    rowOne:$('.col1').text(),
-    rowTwo :$('.col2').text(),
-    rowThree:$('.col3').text(),
-    rowFour:$('.col4').text(),
-    rowFive:$('.col5').text(),
-    rowSix:$('.col6').text(),
-    rowSeven:$('.col7').text(),
-    rowEight:$('.col8').text()
-  }
-
-  for (let key in gameRows){
-    if(gameRows[key] === "XXX"){
-      $('#X').show();
-    }else if(gameRows[key] === "OOO"){
-      $('#Y').show();
+  $(".col").click(function(e) {
+    if (click % 2 === 0) {
+      displayO(e);
+    } else {
+      displayX(e);
     }
+    displayResult();
+    click++
+
+  });
+
+  displayResult = function() {
+
+    let gameRows = {
+      rowOne: $('.col1').text(),
+      rowTwo: $('.col2').text(),
+      rowThree: $('.col3').text(),
+      rowFour: $('.col4').text(),
+      rowFive: $('.col5').text(),
+      rowSix: $('.col6').text(),
+      rowSeven: $('.col7').text(),
+      rowEight: $('.col8').text()
+    }
+
+    for (let key in gameRows) {
+      if (gameRows[key] === "XXX") {
+        $('#X').show();
+        setTimeout("location.href = 'file:///Users/Sandy/wdi/projects/project0/tictac.html'",5000);
+        return;
+      } else if (gameRows[key] === "OOO") {
+        $('#Y').show();
+        setTimeout("location.href = 'file:///Users/Sandy/wdi/projects/project0/tictac.html'",5000);
+        return;
+      }
+    }
+
+    if (click >= 9){
+        $('#draw').show();
+        setTimeout("location.href = 'file:///Users/Sandy/wdi/projects/project0/tictac.html'",5000);
+
+    }
+
   }
- }
-
-
-
-
 });
