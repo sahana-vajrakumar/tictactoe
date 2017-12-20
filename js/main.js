64dc80b4988eb1fd5f49aec06f69f485b664567d
Sahana <sahanakmr32@gmail.com>
2017-12-20 15:10:38 +1100

    player type working

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
+
+
 $(document).ready(function() {
-  let click = 1;
-  let displayX = function(e) {
-    $(event.target).html("X");
-  }
-
-  let displayO = function(e) {
-    $(event.target).html("O");
-  }
-
-  $(".col").click(function(e) {
+let click = 1;
+  let game = function(){
+  $('#game-wrap').show();
+    $(".col").click(function(e) {
     if (click % 2 === 0) {
-      displayO(e);
+      $(event.target).html("X");
     } else {
-      displayX(e);
+      $(event.target).html("O");
     }
     displayResult();
     click++
 
   });
+}
+
 
   displayResult = function() {
 let playerOneScore = 0;
@@ -36,10 +34,12 @@ let playerTwoScore = 0;
     for (let key in gameRows) {
       if (gameRows[key] === "XXX") {
         $('#X').show();
+        playerOneScore++;
         setTimeout("location.href = 'file:///Users/Sandy/wdi/projects/project0/tictac.html'",3000);
         return;
       } else if (gameRows[key] === "OOO") {
         $('#Y').show();
+        playerTwoScore++;
         setTimeout("location.href = 'file:///Users/Sandy/wdi/projects/project0/tictac.html'",3000);
         return;
       }
@@ -63,9 +63,16 @@ $('#gameType').click(function(){
   $('#gameType').hide();
 })
 
-$('#playerType').click(function(){
-  $('#game-wrap').show();
+$('#playerOne').click(function(){
   $('#playerType').hide();
+  click = 2;
+  game();
+})
+
+$('#playerTwo').click(function(){
+  $('#playerType').hide();
+  click = 1;
+  game();
 })
 
 });
