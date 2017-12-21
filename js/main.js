14b2139afe58ad9c083143c981c013d96449033f
Sahana <sahanakmr32@gmail.com>
2017-12-21 14:49:47 +1100

    working version before adding win count and AI

--- js/main.js
+++ js/main.js
@@ -5,13 +5,10 @@ let numberOfClicks = 0;
 let game = function(){
     $(".col").off("click");
     $(".col").one("click" , function(e) {
-
     if (count % 2 === 0) {
       $(event.target).html("X");
-
     } else {
       $(event.target).html("O");
-
     }
     displayResult();
     count++
@@ -40,28 +37,18 @@ let game = function(){
       if (gameRows[key] === "XXX") {
         $('#X').show();
         playerOneScore++;
-
-        // setTimeout("location.href = 'file:///Users/Sandy/wdi/projects/project0/tictac.html'",3000);
-        console.log(`PLAYER ONE SCORE:${playerOneScore}`)
         $('.col').off('click');
         return;
       } else if (gameRows[key] === "OOO") {
         $('#Y').show();
         playerTwoScore++;
-
-        // setTimeout("location.href = 'file:///Users/Sandy/wdi/projects/project0/tictac.html'",3000);
-        console.log(`PLAYER TWO SCORE:${playerTwoScore}`)
         $('.col').off('click');
         return;
       }
     }
-    // debugger;
     if (numberOfClicks >= 8){
-
         $('#draw').show();
         return;
-        // setTimeout("location.href = 'file:///Users/Sandy/wdi/projects/project0/tictac.html'",3000);
-
     }
 
   }
